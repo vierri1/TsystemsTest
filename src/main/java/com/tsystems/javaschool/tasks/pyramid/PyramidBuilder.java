88c4bb26a8da7dd83c482e1f79e06b6b5d3d8fb5
Andrey <avpoponin@yandex.ru>
2018-05-09 21:04:33 +0300

    Added solution pyramid

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -1,5 +1,6 @@
 package com.tsystems.javaschool.tasks.pyramid;
 
+import java.util.Collections;
 import java.util.List;
 
 public class PyramidBuilder {
@@ -14,7 +15,48 @@ public class PyramidBuilder {
      */
     public int[][] buildPyramid(List<Integer> inputNumbers) {
         // TODO : Implement your solution here
-        return new int[0][0];
+        try {
+            Collections.sort(inputNumbers);
+        } catch (OutOfMemoryError ex) {
+            throw new CannotBuildPyramidException();
+        } catch (NullPointerException e) {
+            throw new CannotBuildPyramidException();
+        }
+        int[][] pyramid = null;
+        int dimensionPyramid = 0;
+        int countElements = 0;
+        for (int i = 1; countElements < inputNumbers.size(); i++){
+            countElements = countElements + i;
+            dimensionPyramid++;
+        }
+        if (countElements != inputNumbers.size()) {
+            throw new CannotBuildPyramidException();
+        }
+        else {
+            System.out.println("dimension " + dimensionPyramid);
+            System.out.println("countColumns " + (dimensionPyramid * 2 - 1));
+            int countColumns = dimensionPyramid * 2 -1;
+            pyramid = new int[dimensionPyramid][countColumns];
+            for (int i = 0; i < dimensionPyramid; i++) {
+                for (int j = 0; j < countColumns; j++) {
+                    pyramid[i][j] = 0;
+                }
+            }
+
+            int placeInRow = dimensionPyramid - 1;
+            int countElementsInRow = 1;
+            int listInd = 0;
+            for (int i = 0; i < dimensionPyramid; i++) {
+                for (int j = 0; j < countElementsInRow; j++) {
+                    pyramid[i][placeInRow] = inputNumbers.get(listInd);
+                    listInd++;
+                    placeInRow = placeInRow + 2;
+                }
+                countElementsInRow++;
+                placeInRow = dimensionPyramid - countElementsInRow;
+            }
+        }
+        return pyramid;
     }
 
 
